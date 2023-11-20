b2b5ef7309ff320ffb800ffec400f0b5427c8b47
Dr. Gernot Starke <gs@gernotstarke.de>
2023-11-20 20:43:09 +0100

    add code for GraphQL bug count

--- internal/github/talkToGitHub.go
+++ internal/github/talkToGitHub.go
@@ -2,11 +2,15 @@
 package github
 
 import (
+	"fmt"
 	"github.com/shurcooL/githubv4"
-	"site-usage-statistics/internal/types"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"log"
+	"os"
 )
 
-const GITHUB_GRAPHQL_API_KEY = "GITHUB_PAT"
+const GITHUB_GRAPHQL_API_KEY = "GITHUB_API_KEY"
 
 // Define a query struct
 type IssuesQuery struct {
@@ -17,6 +21,34 @@ type IssuesQuery struct {
 	} `graphql:"repository(owner: $owner, name: $repo)"`
 }
 
-func BugCountForSite(thisSite string, stats *types.SiteStats) {
+func BugCountForSite(thisSite string) {
+	// Set your GitHub API token here
+	apiToken := os.Getenv(GITHUB_GRAPHQL_API_KEY)
+
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: apiToken},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+
+	// Initialize GitHub GraphQL client
+	client := githubv4.NewClient(httpClient)
+
+	// Fill in these variables with the appropriate values
+	variables := map[string]interface{}{
+		"owner": githubv4.String("arc42"),  // Replace with the repository owner's name
+		"repo":  githubv4.String(thisSite), // Replace with the repository name
+	}
+
+	// Declare an instance of the query struct
+	var query IssuesQuery
+
+	// Perform the query
+	err := client.Query(context.Background(), &query, variables)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Output the result
+	fmt.Printf("Number of open issues: %d\n", query.Repository.Issues.TotalCount)
 
 }
