005248736e53c25915bc9010f118ac3a5c0b2d89
Dr. Gernot Starke <gs@gernotstarke.de>
2023-12-27 12:39:29 +0100

    added small scripts which don't work for Goland env settings

---
+++ go-app/show-pwd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo
+echo
+echo "show-pwd v 1.0 (Dec. 27th 2023)"
+echo "working directory is: " $PWD
+echo
+echo "==============================="
---
+++ go-app/source-env-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ./set-api-keys.sh
+
+## the set-api-keys.sh file contains secrets, therefore it is NOT
+## contained in git.
